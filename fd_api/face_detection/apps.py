97dd76f21993ab5739b252714f220b12111c41de
Illia-tsar <reklamamicloy@gmail.com>
2023-03-24 02:35:53 +0200

    added FaceDetectionConfig

--- fd_api/face_detection/apps.py
+++ fd_api/face_detection/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+import mediapipe as mp
 
 
 class FaceDetectionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "face_detection"
+
+    predictor = mp.solutions.face_detection.FaceDetection(
+        model_selection=1,
+        min_detection_confidence=.5
+    )
