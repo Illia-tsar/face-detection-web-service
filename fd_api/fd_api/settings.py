f9ed45f281c4861f08142822418bb32f0015c1b1
Illia-tsar <reklamamicloy@gmail.com>
2023-03-24 02:33:31 +0200

    upd settings

--- fd_api/fd_api/settings.py
+++ fd_api/fd_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vdr7q1ko_)26^%!4tw=8=h$he9665l$-pm(@cw#hy$pyi2prfw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "face_detection",
 ]
 
 MIDDLEWARE = [
