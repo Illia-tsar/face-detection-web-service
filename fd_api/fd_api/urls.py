2c295d2182c82a6cbf1c2af62ceb69ac6f06a0a4
Illia-tsar <reklamamicloy@gmail.com>
2023-03-24 02:40:41 +0200

    added detection endpoint

--- fd_api/fd_api/urls.py
+++ fd_api/fd_api/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from face_detection import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("model/", views.detect)
 ]
